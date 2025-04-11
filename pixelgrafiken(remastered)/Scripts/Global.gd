a0d124180507e30fd7cd5f3871350e68425fc870
LSon <leosonnenberg515@gmail.com>
2025-04-11 15:53:06 +0200

    Added Pixelgrafiken minigame

---
+++ pixelgrafiken(remastered)/Scripts/Global.gd
@@ -0,0 +1,4 @@
+extends Node
+
+var Options: int
+var Tex: ImageTexture
---
+++ pixelgrafiken(remastered)/Scripts/auswahl.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+
+func _on_button_pressed() -> void:
+	Global.Options = 1
+	SceneSwitcher.switch_scene("res://Scenes/Game.tscn")
+
+
+func _on_button_2_pressed() -> void:
+	Global.Options = 2
+	SceneSwitcher.switch_scene("res://Scenes/Game.tscn")
