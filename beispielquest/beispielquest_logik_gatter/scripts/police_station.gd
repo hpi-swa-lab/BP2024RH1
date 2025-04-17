31433c4c306e4e6cdf81ce1fccf19967fe56b0f8
Lara-S-K <114135998+MineKecks@users.noreply.github.com>
2025-04-17 17:02:17 +0200

    introduction level logik gatter started
    created lv to explain how logik gatter works
    logik gatter are in there, the introduction not

--- beispielquest/beispielquest_logik_gatter/scripts/police_station.gd
+++ beispielquest/beispielquest_logik_gatter/scripts/police_station.gd
@@ -7,6 +7,7 @@ var Qestionmark_Found: bool = false
 @onready var dark_mode_blackout: Sprite2D = $Dark_Mode_Blackout
 
 
+
 func _on_helpsys_questionmark_found() -> void:
 	if Qestionmark_Found:
 		timer_blackout_beginn.start()
@@ -21,4 +22,9 @@ func _on_timer_blackout_beginn_timeout() -> void:
 
 func _on_tech_room_electricity_box_pressed() -> void:
 	if dark_mode_blackout.visible:
-		print("start mini game")	#hier minigame 1 starten
+		print("start mini game")	
+		#maybe erst dialog kurz das etwas mit dem stromkasten nicht stimmt und verbindungen repariert werden müssen also hier dann erst dialog starten und wait fkt bis dialog fertig dann minigame starten 
+		
+		
+		#hier minigame 1 starten
+		get_tree().change_scene_to_file("res://scenes/Minigames/Logik_Gatter_level_introduction.tscn")
