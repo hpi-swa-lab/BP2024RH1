c337b29226d63f8b6b25a987c530b6a6aec4d542
Lara-S-K <114135998+MineKecks@users.noreply.github.com>
2025-04-22 14:38:53 +0200

    ende von introduction logikgatter mit übergang zu minigame 1 implementiert

--- beispielquest/beispielquest_logik_gatter/scripts/logik_gatter_level_introduction.gd
+++ beispielquest/beispielquest_logik_gatter/scripts/logik_gatter_level_introduction.gd
@@ -2,6 +2,8 @@ extends Control
 
 signal start_1
 signal start_2
+signal ziel_input_true
+signal ziel_input_false
 
 
 func _on_start_and_left_1_pressed() -> void:
@@ -9,3 +11,9 @@ func _on_start_and_left_1_pressed() -> void:
 
 func _on_start_and_left_2_pressed() -> void:
 	emit_signal("start_2")
+
+func _on_ziel_ziel_input_false() -> void:
+	emit_signal("ziel_input_false")
+
+func _on_ziel_ziel_input_true() -> void:
+	emit_signal("ziel_input_true")
--- beispielquest/beispielquest_logik_gatter/scripts/minigame_introduction.gd
+++ beispielquest/beispielquest_logik_gatter/scripts/minigame_introduction.gd
@@ -1,5 +1,6 @@
 extends Control
 
+var ziel_input: bool = false
 
 var explanation: Array[Label] 
 
@@ -13,7 +14,9 @@ var explanation: Array[Label]
 
 @onready var next_button: Button = $next_button
 @onready var back_button: Button = $back_button
+@onready var end_button: Button = $end_button
 
+@onready var good_job: TextureRect = $Good_job
 
 func _ready() -> void:
 	explanation = [
@@ -26,7 +29,9 @@ func _ready() -> void:
 		explanation_end
 	]
 
-
+func _process(delta: float) -> void:
+	if explanation_end.visible:
+		end_button.show()
 
 
 func _on_next_button_pressed() -> void:
@@ -66,3 +71,26 @@ func _on_logik_gatter_level_introduction_start_2() -> void:
 	if explanation_try_start_2.visible:
 			explanation_try_start_2.hide()
 			explanation_gatter.show()
+
+
+func _on_end_button_pressed() -> void:
+	if ziel_input:
+		# text das es geschaft wurde
+		explanation_end.text = "Du hast es geschafft das Signal ist beim ZIel angekommen"
+		#übergang zum richtigen minispiel
+		good_job.show()
+		explanation_end.hide()
+	else: 
+		#text das das signal noch nicht beim ziel ist
+		explanation_end.text = "Das Signal ist noch nicht beim Ziel angekommen"
+
+
+func _on_logik_gatter_level_introduction_ziel_input_false() -> void:
+	ziel_input = false
+
+func _on_logik_gatter_level_introduction_ziel_input_true() -> void:
+	ziel_input = true
+
+
+func _on_start_minigame_1_pressed() -> void:
+			get_tree().change_scene_to_file("res://scenes/Minigames/Minigame_1.tscn")
