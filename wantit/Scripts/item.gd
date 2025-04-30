1bfe96b3ae9cc7fad2c934bacb8871ff58bc51b3
sarahWolf <Sarah.Wolf@student.hpi.uni-potsdam.de>
2025-04-30 16:41:31 +0200

    introduction case scenes WIP

---
+++ wantit/Scripts/item.gd
@@ -0,0 +1,19 @@
+extends TextureButton
+
+@export var is_collectible: bool = true
+
+@export var dialogue_resource: Resource
+@export var dialogue_start: String
+
+func _ready() -> void:
+	if texture_normal:
+		var image: Image = texture_normal.get_image()
+		var bitmap: BitMap = BitMap.new()
+		bitmap.create_from_image_alpha(image)
+		texture_click_mask = bitmap
+	
+func _pressed() -> void:
+	print("You clicked on an item!")
+	DialogueManager.show_dialogue_balloon(dialogue_resource, dialogue_start)
+	if is_collectible:
+		queue_free()
