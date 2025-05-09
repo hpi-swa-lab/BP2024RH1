8dc26a5f84c101af39b260997914d421c61114ad
LSon <leosonnenberg515@gmail.com>
2025-05-09 17:22:29 +0200

    Changed Icons on Map and logic for them

--- wantit/Scripts/map.gd
+++ wantit/Scripts/map.gd
@@ -5,22 +5,21 @@ func _ready() -> void:
 	#var Library = CaseManager.Location.new(preload("res://Assets/library.png"), Vector2(0.1, 0.1), Vector2(-150, 40), preload("res://Scenes/testscene.tscn"))
 	#add_location(Library) # Its not the right Scene for the Library should be changed
 
-	# Loads next Location
 	if Globals.nextScene != null:
 		add_location(Globals.nextScene)
 	
-func add_location(Location: CaseManager.Location):	#Type of Loc_scene should be changed later once scene switching logic is implemented
-	var newLocation = Button.new()
-	var Style = StyleBoxEmpty.new()
+func add_location(Location: CaseManager.Location):
+	var newLocation = TextureButton.new()
 	
-	newLocation.position = Location.LocationPos
-	newLocation.scale = Location.LocationScale
-	newLocation.icon = Location.LocationTex
+	newLocation.scale = %Map.scale
+	newLocation.position = Vector2(0, -27) # So that it overlaps with the map
+	newLocation.texture_normal = Location.LocationTex
 	
-	newLocation.add_theme_stylebox_override("hover", Style)
-	newLocation.add_theme_stylebox_override("normal", Style)
-	newLocation.add_theme_stylebox_override("pressed", Style)
-	newLocation.add_theme_stylebox_override("focus", Style)
+	if newLocation.texture_normal:
+		var image: Image = newLocation.texture_normal.get_image()
+		var bitmap: BitMap = BitMap.new()
+		bitmap.create_from_image_alpha(image)
+		newLocation.texture_click_mask = bitmap
 	
 	newLocation.pressed.connect(self.switch_location.bind(Location.LocationScene))
 	%Control.add_child(newLocation)
