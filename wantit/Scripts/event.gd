55129de4713b819c6e2a36493addc9ef829efa46
LSon <leosonnenberg515@gmail.com>
2025-06-26 13:27:16 +0200

    Most of it is working again, also changed events so each event can only be calles once

--- wantit/Scripts/event.gd
+++ wantit/Scripts/event.gd
@@ -5,8 +5,9 @@ class_name  Event
 @export var event_name: String
 @export var location_name: String
 @export var start_condition: Condition
+var has_started: bool = false
 
 func is_valid(player_items: Array) -> bool:
-	if start_condition:
+	if start_condition and not has_started:
 		return start_condition.is_satisfied(player_items)
 	return false
