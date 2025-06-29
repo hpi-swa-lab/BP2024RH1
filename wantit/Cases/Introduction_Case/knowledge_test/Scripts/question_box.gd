6cbcbf9fe018e91a8e6a8dbfd6513a5a2ff4cb6f
KarlDuda <karl.duda@web.de>
2025-06-29 19:54:19 +0200

    added sliding likert scale, and picture suppport for questions

--- wantit/Cases/Introduction_Case/knowledge_test/Scripts/question_box.gd
+++ wantit/Cases/Introduction_Case/knowledge_test/Scripts/question_box.gd
@@ -2,6 +2,8 @@ extends VBoxContainer
 
 @onready var question_label: Label = $QuestionLabel
 @onready var answer_buttons: Array = [$A1, $A2, $A3, $A4]
+@onready var question_image: TextureRect = $QuestionImage
+
 
 signal answer_chosen(correct: bool)
 
@@ -12,13 +14,28 @@ func _ready():
 
 func show_question(q: Dictionary):
 	question_label.text = q["question"]
+
+	# Show or hide image
+	if q.has("image") and q["image"] != "":
+		var image_path = "res://Cases/Introduction_Case/knowledge_test/Assets/"+q["image"]  # e.g., "res://images/question1.png"
+		var texture = load(image_path)
+		if texture:
+			question_image.texture = texture
+			question_image.visible = true
+		else:
+			question_image.visible = false
+	else:
+		question_image.visible = false
+
+	# Setup answer buttons
 	for i in range(4):
 		var btn = answer_buttons[i]
 		btn.visible = true
 		btn.text = q["answers"][i]
-		if (btn.text == ""):
+		if btn.text == "":
 			btn.visible = false
 		btn.is_correct = (i == q["correct"])
+
 
 func _on_answer_selected(correct: bool):
 	answer_chosen.emit(correct)
---
+++ wantit/Cases/Introduction_Case/knowledge_test/Scripts/sliding_game_survey.gd
@@ -0,0 +1,31 @@
+# main.gd
+extends Node2D
+
+@onready var slider_box = $QuestionWrap/SliderBox
+#@onready var score_label = $ScoreLabel
+#@onready var timer = $Timer
+
+var score = []
+var current_question = 0
+var questions = []
+
+func _ready():
+	load_questions()
+	show_next_question()
+	slider_box.connect("answer_chosen", Callable(self, "_on_answer_chosen"))
+
+
+func load_questions():
+	var file = FileAccess.open("res://Cases/Introduction_Case/knowledge_test/Assets/survey_questions.json", FileAccess.READ)
+	questions = JSON.parse_string(file.get_as_text())
+
+func show_next_question():
+	if current_question >= questions.size():
+		print("Quiz Finished! Final score: ", score)
+		return
+	slider_box.show_question(questions[current_question])
+
+func _on_answer_chosen(value):
+	score.append(value) 
+	current_question += 1
+	show_next_question()
