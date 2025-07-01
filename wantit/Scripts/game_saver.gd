e73187a34558d776c64f43d5f2f1f9a333eb63c9
naida93 <ekanaida@gmail.com>
2025-07-01 16:13:26 +0200

    wip: trying to save using browser localStorage

--- wantit/Scripts/game_saver.gd
+++ wantit/Scripts/game_saver.gd
@@ -2,32 +2,67 @@ extends RefCounted
 class_name GameSaver
 
 const SAVE_PATH := "user://progress.json"
+const SAVE_KEY := "my_game_save_data"
 
 func save_game(game: Game):
 	var save_data = game.get_save_data()
-	var file = FileAccess.open(SAVE_PATH, FileAccess.WRITE)
-	file.store_string(JSON.stringify(save_data, "\t"))
-	file.flush()
-	file.close()
 	
 	if OS.has_feature("HTML5"):
-		JavaScriptBridge.eval("FS.syncfs(false, function(err) { console.log('Save synced:', err); });")
+		_save_to_local_storage(SAVE_KEY, save_data)
+	else:
+		var file = FileAccess.open(SAVE_PATH, FileAccess.WRITE)
+		file.store_string(JSON.stringify(save_data, "\t"))
+		file.flush()
+		file.close()
+	#if OS.has_feature("HTML5"):
+		#JavaScriptBridge.eval("FS.syncfs(false, function(err) { console.log('Save synced:', err); });")
 	
 func load_saved_game_data(game: Game):
+	#if OS.has_feature("HTML5"):
+		#JavaScriptBridge.eval("FS.syncfs(true, function(err) { console.log('Load synced:', err); });")
+	#
+	#if not FileAccess.file_exists(SAVE_PATH):
+		#print("Save file not found.")
+		#return
+	#
+	#var file = FileAccess.open(SAVE_PATH, FileAccess.READ)
+	#var content = file.get_as_text()
+	#file.close()
+	var loaded_data = {}
+	
 	if OS.has_feature("HTML5"):
-		JavaScriptBridge.eval("FS.syncfs(true, function(err) { console.log('Load synced:', err); });")
+		loaded_data = _load_from_local_storage(SAVE_KEY)
+	else:
+		if not FileAccess.file_exists(SAVE_PATH):
+			print("Save file not found.")
+			return
+		var file = FileAccess.open(SAVE_PATH, FileAccess.READ)
+		var content = file.get_as_text()
+		file.close()
+		var result = JSON.parse_string(content)
+		if typeof(result) == TYPE_DICTIONARY:
+			loaded_data = result
 	
-	if not FileAccess.file_exists(SAVE_PATH):
-		print("Save file not found.")
-		return
-	
-	var file = FileAccess.open(SAVE_PATH, FileAccess.READ)
-	var content = file.get_as_text()
-	file.close()
-	
-	var result = JSON.parse_string(content)
-	if typeof(result) != TYPE_DICTIONARY:
-		print("Failed to parse save file.")
-		return
-	
-	game.saved_game_data = result
+	if loaded_data.size() > 0:
+		game.saved_game_data = loaded_data
+	else:
+		print("No saved data found.")
+
+
+func _save_to_local_storage(key: String, data: Dictionary) -> void:
+	if OS.has_feature("HTML5"):
+		var json_string = JSON.stringify(data)
+		var js_code = "localStorage.setItem('" + key + "', '" + json_string.replace("'", "\\'") + "');"
+		JavaScriptBridge.eval(js_code)
+
+func _load_from_local_storage(key: String) -> Dictionary:
+	var js_code = "localStorage.getItem('" + key + "');"
+	var result = JavaScriptBridge.eval(js_code)
+
+	if result == null or result == "":
+		return {}
+
+	var parse_result = JSON.parse_string(result)
+	if typeof(parse_result) == TYPE_DICTIONARY:
+		return parse_result
+	return {}
