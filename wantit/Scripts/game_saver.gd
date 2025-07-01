f27ab69e623f94ece6bfba9936f58897056ec322
naida93 <ekanaida@gmail.com>
2025-07-01 15:07:14 +0200

    changes saving progress

--- wantit/Scripts/game_saver.gd
+++ wantit/Scripts/game_saver.gd
@@ -7,9 +7,16 @@ func save_game(game: Game):
 	var save_data = game.get_save_data()
 	var file = FileAccess.open(SAVE_PATH, FileAccess.WRITE)
 	file.store_string(JSON.stringify(save_data, "\t"))
+	file.flush()
 	file.close()
 	
+	if OS.has_feature("HTML5"):
+		JavaScriptBridge.eval("FS.syncfs(false, function(err) { console.log('Save synced:', err); });")
+	
 func load_saved_game_data(game: Game):
+	if OS.has_feature("HTML5"):
+		JavaScriptBridge.eval("FS.syncfs(true, function(err) { console.log('Load synced:', err); });")
+	
 	if not FileAccess.file_exists(SAVE_PATH):
 		print("Save file not found.")
 		return
