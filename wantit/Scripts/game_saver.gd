b28d1a6c77ced8d68fc7eadd1d6ad0d223c3bf5b
naida93 <ekanaida@gmail.com>
2025-07-02 15:35:44 +0200

    completed cases buttons have different color in case overview

--- wantit/Scripts/game_saver.gd
+++ wantit/Scripts/game_saver.gd
@@ -14,20 +14,8 @@ func save_game(game: Game):
 		file.store_string(JSON.stringify(save_data, "\t"))
 		file.flush()
 		file.close()
-	#if OS.has_feature("HTML5"):
-		#JavaScriptBridge.eval("FS.syncfs(false, function(err) { console.log('Save synced:', err); });")
 	
 func load_saved_game_data(game: Game):
-	#if OS.has_feature("HTML5"):
-		#JavaScriptBridge.eval("FS.syncfs(true, function(err) { console.log('Load synced:', err); });")
-	#
-	#if not FileAccess.file_exists(SAVE_PATH):
-		#print("Save file not found.")
-		#return
-	#
-	#var file = FileAccess.open(SAVE_PATH, FileAccess.READ)
-	#var content = file.get_as_text()
-	#file.close()
 	var loaded_data = {}
 	
 	if OS.has_feature("HTML5"):
