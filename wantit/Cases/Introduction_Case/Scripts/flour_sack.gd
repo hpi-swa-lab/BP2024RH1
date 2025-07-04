5a9128610baef3ca7e9907d2d89163208c977937
naida93 <ekanaida@gmail.com>
2025-07-04 13:29:15 +0200

    fixes tiny bug from previous commit

--- wantit/Cases/Introduction_Case/Scripts/flour_sack.gd
+++ wantit/Cases/Introduction_Case/Scripts/flour_sack.gd
@@ -4,8 +4,8 @@ func _ready():
 	super._ready()
 
 func _on_flour_sack_pressed() -> void:
-	if not is_found:
-		set_found()
+	#if not is_found:
+	set_found()
 
 func set_found() -> void:
 	%"Flour Sack".z_index = 0
