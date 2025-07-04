5250e2fc35f46516913f502192de22c077635b44
naida93 <ekanaida@gmail.com>
2025-07-04 13:11:04 +0200

    floursack could be clicked only once

--- wantit/Cases/Introduction_Case/Scripts/flour_sack.gd
+++ wantit/Cases/Introduction_Case/Scripts/flour_sack.gd
@@ -5,6 +5,10 @@ func _ready():
 
 func _on_flour_sack_pressed() -> void:
 	if not is_found:
-		%"Flour Sack".z_index = 0
-		%"Flour Sack".disabled = true
+		set_found()
+
+func set_found() -> void:
+	%"Flour Sack".z_index = 0
+	%"Flour Sack".disabled = true
+	if not %Key.is_found:
 		%Key.show()
