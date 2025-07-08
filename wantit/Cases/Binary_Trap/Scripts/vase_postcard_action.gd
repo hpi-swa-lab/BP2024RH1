c2180eb2a1f6128d32beae962b920d53c272e17c
sarahWolf <Sarah.Wolf@student.hpi.uni-potsdam.de>
2025-07-08 15:39:37 +0200

    action script for postcard and vase still not working

--- wantit/Cases/Binary_Trap/Scripts/vase_postcard_action.gd
+++ wantit/Cases/Binary_Trap/Scripts/vase_postcard_action.gd
@@ -1,17 +1,17 @@
 extends  Item
 
-var location: Location
+var extended_item: Item
 	
 func _ready() -> void:
 	create_overlay()
-	
-	#for child in get_parent().get_children():
-	#	if child is Location:
-	#		location.item_found.emit()
-	#
 	
 func create_overlay() -> void:
 	var overlay: CanvasLayer = load("res://Cases/Binary_Trap/Scenes/item_overlay.tscn").instantiate()
 	overlay.item = self.texture_normal
 	
 	get_parent().add_child(overlay)
+
+	#for child in get_parent().get_children():
+	#	if child is Location:
+	#		print("CHild is Location!!", child, extended_item)
+	#		child.item_found.emit(extended_item, child)
